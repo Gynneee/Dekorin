2957c11b82fe0170d22d6259f24c9395a7bb2b10
Daryl <daryllim960@gmail.com>
2025-10-13 13:27:55 +0700

    dropdown

--- script.js
+++ script.js
@@ -65,10 +65,7 @@ menuItems.forEach(item => {
   item.addEventListener('click', () => {
     menuItems.forEach(i => i.classList.remove('active'));
     item.classList.add('active');
-
-    console.log(`${item.textContent.trim()} clicked`);
   });
 });
-
 
 });
