1eccb16cbd7c3feb6356f8fed9a12fb06762a063
fadhil <fadhileka642@gmail.com>
2025-10-29 07:49:42 +0700

    payment-detail and payment-success

--- cart.js
+++ cart.js
@@ -1,8 +1,39 @@
 const checkboxes = document.querySelectorAll(".select-box");
 const checkout = document.getElementById("checkout");
+const minusButtons = document.querySelectorAll(".qty-btn.minus");
+const popup = document.getElementById("removePopup");
+const cancelBtn = document.getElementById("cancelRemove");
+const confirmBtn = document.getElementById("confirmRemove");
 
+// Toggle centang
 function toggleSelect(btn) {
   btn.classList.toggle("active");
   const selected = document.querySelectorAll(".select-box.active");
   checkout.classList.toggle("show", selected.length > 0);
 }
+
+// Tambah & kurang qty
+function increaseQty(btn) {
+  const qtySpan = btn.previousElementSibling;
+  qtySpan.textContent = parseInt(qtySpan.textContent) + 1;
+}
+
+function decreaseQty(btn) {
+  const qtySpan = btn.nextElementSibling;
+  const current = parseInt(qtySpan.textContent);
+  if (current > 1) {
+    qtySpan.textContent = current - 1;
+  } else {
+    popup.classList.remove("hidden");
+  }
+}
+
+// Popup remove
+cancelBtn.addEventListener("click", () => {
+  popup.classList.add("hidden");
+});
+
+confirmBtn.addEventListener("click", () => {
+  popup.classList.add("hidden");
+  alert("Item removed from cart! (simulasi)");
+});
